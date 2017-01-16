220423a2d65d41c60fcbee711e54358a52754b35
matthew jenkins <drakaven@hotmail.com>
2017-01-16 16:28:54 +0000

    step 4

---
+++ callback.js
@@ -0,0 +1,35 @@
+const https = require("https");
+
+const getAndPrintHTML = function (options, callback) {
+
+  /* Add your code here */
+  var requestOptions = {
+    host: options.host,
+    path: options.path
+  };
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+
+    var chunks = "";
+    /* Add your code here */
+    response.on('data', function(data) {
+      chunks += data;
+    });
+
+    response.on('end', function() {
+      callback(chunks);
+    })
+  });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getAndPrintHTML(requestOptions, printHTML);
+
+function printHTML (html) {
+  console.log(html);
+}
